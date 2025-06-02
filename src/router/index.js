8dc854e5271f86fc4d42ef79f8e00165701ac80b
Abdullah FIVE <82946724+Abdullah2690@users.noreply.github.com>
2025-06-02 10:13:03 +0200

    fix: change router history mode to use hash for better compatibility

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import WorksView from '@/views/WorksView.vue'
 import WorkDetailView from '@/views/WorkDetailView.vue'
@@ -30,7 +30,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(), // ← changed this line
   routes
 })
 
